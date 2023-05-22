0b3ef8bf0d1efe3a489a1d0144681ddc1af0cb2b
TomCabral <apbc@cesar.school>
2023-05-22 19:02:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,107 +1,45 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
-
-int gerarNumeroAleatorio(int min, int max) {
-    int numero = (rand() % (max - min + 1)) + min;
-    return numero;
-}
-
-typedef struct personagem{
-    char nome[50];
-    int hp;
-    int ataque;
-    int pm;
-    struct personagem *prox;
-} personagem;
-
-typedef struct inimigo{
-    char nome[50];
-    int hp;
-    int ataque;
-    int pm;
-    int id;
-    struct inimigo *proxI;
-} inimigo;
-
-//insere o um heroi em uma lista simplesmente encadeada 
-void inserirH(personagem **head, int hp, int ataque, int pm, char nome[50]) {
-  personagem *novo = (personagem *) malloc(sizeof(personagem));
-  if (novo != NULL){
-    strcpy(novo->nome, nome);  
-    novo->hp = hp;
-    novo->ataque = ataque;
-    novo->pm = pm;
-    novo->prox = *head;
-    *head = novo;
-  }
-}
-//insere um inimigo em uma lista simplesmente encadeada
-void inserirI(inimigo **headI, int hp, int ataque, int pm, char nome[50]) {
-  inimigo *novoI = (inimigo *) malloc(sizeof(inimigo));
-  if (novoI != NULL){
-    int id = gerarNumeroAleatorio(1, 40);
-    strcpy(novoI->nome, nome);  
-    novoI->hp = hp;
-    novoI->ataque = ataque;
-    novoI->pm = pm;
-    novoI->id = id;
-    novoI->proxI = *headI;
-    *headI = novoI;
-  }
-}
-
-//imprime a lista de herois
-void imprimirH(personagem *head) {
-  if (head == NULL)
-    printf("\nErro na inserção de personagens.");  
-  else {
-    while (head != NULL) {
-    printf("Nome: %s\n HP: %d\n Ataque: %d\n PM: %d\n\n", head->nome, head->hp, head->ataque, head->pm);
-      head = head->prox;
-    }  
-  }
-}
-
-
-
+#include "lib.h"
 
 int main() {
     srand(time(NULL));
     personagem *head = NULL;
     inimigo *headI = NULL;
 
-    
-    //Declaração dos personagens
+    // Declaração dos personagens
     inserirH(&head, 100, 55, 100, "Guerreiro Infernal");
     inserirH(&head, 80, 32, 140, "Mago de Gelo");
     inserirH(&head, 90, 40, 120, "Arqueira florestal");
-  
-    //Declaração dos inimigos
-    inserirI(&head, 140, 4, 15, gerarNumeroAleatorio(1, 40),"Lamina das Sombras");
-    inserirI(&head, 100, 8, 12, gerarNumeroAleatorio(1, 40),"Cavaleiro Espectral");
-    inserirI(&head, 170, 6, 20, gerarNumeroAleatorio(1, 40),"Servo do Abismo");
-    inserirI(&head, 200, 24, 40,"Arauto do Crepúsculo");
-    inserirI(&head, 400, 2, 6, gerarNumeroAleatorio(1, 40),"Guardião das Trevas");
-    
 
-  
-      inserirI(&headI, 220, 24, 17, "Artorias");
+    // Declaração dos inimigos
+    inserirI(&headI, 140, 4, 15, gerarNumeroAleatorio(1, 40), "Lamina das Sombras");
+    inserirI(&headI, 100, 8, 12, gerarNumeroAleatorio(1, 40), "Cavaleiro Espectral");
+    inserirI(&headI, 170, 6, 20, gerarNumeroAleatorio(1, 40), "Servo do Abismo");
+    inserirI(&headI, 200, 24, 40, "Arauto do Crepúsculo");
+    inserirI(&headI, 400, 2, 6, gerarNumeroAleatorio(1, 40), "Guardião das Trevas");
+    inserirI(&headI, 220, 24, 17, gerarNumeroAleatorio(1, 40), "Artorias");
+    inserirI(&headI, 230, 27, 16, gerarNumeroAleatorio(1, 40), "espreitador sombrio");
+    inserirI(&headI, 240, 26, 18, gerarNumeroAleatorio(1, 40), "Legião ardente");
+    inserirI(&headI, 250, 29, 19, gerarNumeroAleatorio(1, 40), "Soldado do desespero");
+    inserirI(&headI, 270, 30, 20, gerarNumeroAleatorio(1, 40), "Lorde das cinzas gwyn");
 
-inserirI(&headI, 230, 27, 16,  "Espreitador sombrio");
+    // Sort the enemies based on their ID using Insertion Sort
+    insertionSort(&headI);
 
-inserirI(&headI, 240, 26, 18, "Legião ardente");
-
-inserirI(&headI, 250, 29, 19, "Soldado do desespero");
-
-inserirI(&headI, 270, 30, 20, "Lorde das cinzas gwyn");
-        
-   
-
-
-  
-   //Exibição dos personagens 
+    // Exibição dos personagens
+    printf("Lista de Personagens:\n");
     imprimirH(head);
+
+    // Exibição dos inimigos ordenados por ID
+    printf("Lista de Inimigos (ordenados por ID):\n");
+    inimigo *tempI = headI;
+    while (tempI != NULL) {
+        printf("ID: %d\nNome: %s\nHP: %d\nAtaque: %d\nPM: %d\n\n", tempI->id, tempI->nome, tempI->hp, tempI->ataque, tempI->pm);
+        tempI = tempI->proxI;
+    }
+
     return 0;
 }

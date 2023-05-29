faebd062da8739b8940b9ad3ab2f90932788e232
TomCabral <apbc@cesar.school>
2023-05-28 23:15:54 -0300

    Inserção de varias novas funções.

--- lib.h
+++ lib.h
@@ -2,31 +2,45 @@
 #define LIB_H
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
+#define MAX_INIMIGOS 10
+
 typedef struct personagem {
-    char nome[50];
-    int hp;
-    int ataque;
-    int pm;
-    struct personagem *prox;
+  char nome[50];
+  int hp;
+  int ataque;
+  int pm;
+  struct personagem *prox;
 } personagem;
 
 typedef struct inimigo {
-    char nome[50];
-    int hp;
-    int ataque;
-    int pm;
-    int id;
-    struct inimigo *proxI;
+  char nome[50];
+  int hp;
+  int ataque;
+  int pm;
+  int id;
+  struct inimigo *proxI;
 } inimigo;
 
 int gerarNumeroAleatorio(int min, int max);
 void inserirH(personagem **head, int hp, int ataque, int pm, char nome[50]);
-void inserirI(inimigo **headI, int hp, int ataque, int pm, int id, char nome[50]);
+void inserirI(inimigo **headI, int hp, int ataque, int pm, int id,
+              char nome[50]);
 void imprimirH(personagem *head);
-void insertionSort(inimigo **headI);
+void imprimirI(inimigo *headI);
+void ataque(personagem *personagemAtual, inimigo *inimigoAtual);
+void ataqueMagico(personagem *personagemAtual, inimigo *inimigoAtual);
+void defesa(personagem *personagemAtual, inimigo *inimigoAtual);
+void removerP(personagem **head, char nome[50]);
+void removerI(inimigo **headI, int id);
+personagem *escolherPersonagem(int escolha);
+void insertionSort(int arr[], int n);
+void liberarMemoriaH(personagem *head);
+void liberarMemoriaI(inimigo *head);
+inimigo *buscarI(inimigo *head, int id);
+inimigo *escolherInimigoAleatorio(inimigo *headI);
 
-#endif  // LIB_H
+#endif // LIB_H
